cee2b229441d8f074de25f204d9f89f9b370c49a
Jorden Hilton <jorden.hilton@gmail.com>
2020-07-14 14:20:42 -0500

    Hook up adding a note

--- Notes-app/app.js
+++ Notes-app/app.js
@@ -1,7 +1,9 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 const { demandOption } = require("yargs");
+
+yargs.version("1.1.0");
 
 // Create add command
 yargs.command({
@@ -20,8 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title: " + argv.title);
-    console.log("Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
@@ -29,8 +30,15 @@ yargs.command({
 yargs.command({
   command: "remove",
   describe: "Remove a note",
-  handler: function () {
-    console.log("Removing the note");
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
