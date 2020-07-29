f05f4270f640149b5bec47667e7b372722f932dd
Jorden Hilton <jorden.hilton@gmail.com>
2020-07-29 18:14:32 -0500

    Serving up HTML &  JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,24 +3,26 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("help page");
+  res.send({
+    name: "Jorden",
+    age: 32,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("about page");
+  res.send("<h1>About</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Current Conditions");
+  res.send({
+    forecast: "It is snowing",
+    location: "St. Louis",
+  });
 });
-
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
   console.log("Server is up on port 300.");
