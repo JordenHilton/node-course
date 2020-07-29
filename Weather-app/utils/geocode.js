eb16c3bfe651431e31ad2b486313a9912fb3fbd7
Jorden Hilton <jorden.hilton@gmail.com>
2020-07-29 15:09:49 -0500

    Destructuring and object property short hand

--- Weather-app/utils/geocode.js
+++ Weather-app/utils/geocode.js
@@ -6,16 +6,16 @@ const geocode = (address, callback) => {
     encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1Ijoiam1oaWx0b24iLCJhIjoiY2tjdGZhNHpjMXkyYzJ5bW91dWg4aW5kaSJ9.LMkqpNEbSrP68S5zXYDFaw&limit=1";
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Try another search", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[0],
+        longitude: body.features[0].center[1],
+        location: body.features[0].place_name,
       });
     }
   });
